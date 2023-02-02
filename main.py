e2195c3a95b589189769b8ca132dbc0ac4e73a8f
Adrian <a.jurkowski@interia.eu>
2023-02-02 14:58:27 +0100

    Update main.py
    dodanie zapętlenia

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 print("--Witam w prostym Kalkulatorze--")
-print("Podaj dwie liczby a następnie wybierz z jakiego działania chcesz skorzystać.")
-print("1. Dodawanie")
-print("2. Odejmowanie")
-print("3. Mnożenie")
-print("4. Dzielenie")
+while True:
+    print()
+    print("Podaj dwie liczby a następnie wybierz z jakiego działania chcesz skorzystać.")
+    print("1. Dodawanie")
+    print("2. Odejmowanie")
+    print("3. Mnożenie")
+    print("4. Dzielenie")
+    print()
 
-a = int(input("Podaj pierwszą liczbę całkowitą a: "))
-b = int(input("Podaj drugą liczbę całkowitą b: "))
+    a = int(input("Podaj pierwszą liczbę całkowitą a: "))
+    b = int(input("Podaj drugą liczbę całkowitą b: "))
 
-action = input("Wybierz formę działania za pomocą cyfry od 1 do 4: ")
+    action = input("Wybierz formę działania za pomocą cyfry od 1 do 4: ")
 
-if action == "1":
-    print(a,"+", b,"=", a + b)
-elif action == "2":
-    print(a,"-", b,"=", a - b)
-elif action == "3":
-    print(a,"*", b,"=", a * b)
-elif action == "4":
-    print(a,"/", b, "=", a / b)
-else:
-    print("Niepoprawna cyfra. Wybierz z przedziału 1-4: ")
+    if action == "1":
+        print(a,"+", b,"=", a + b)
+    elif action == "2":
+        print(a,"-", b,"=", a - b)
+    elif action == "3":
+        print(a,"*", b,"=", a * b)
+    elif action == "4":
+        print(a,"/", b, "=", a / b)
+    else:
+        print("Niepoprawna cyfra. Wybierz z przedziału 1-4: ")
